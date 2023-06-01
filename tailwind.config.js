3414efd397550e6348975d754479b5e3b0aebdd5
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-06-01 22:36:38 +0530

    feat-nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         "color-primary": "#01051e",
         "color-primary-light": "#020726",
         "color-primary-dark": "#010417",
@@ -12,13 +12,12 @@ module.exports = {
         "color-white": "#fff",
         "color-blob": "#A427DF",
       }
-
     },
-    container:{
-      center:true,
-      padding:{
-        default: '20px',
-        md:"50px"
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        md: "50px"
       }
     }
   },
